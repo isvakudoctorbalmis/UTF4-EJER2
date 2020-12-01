795884331617a2148e9affe7fdc326b0094120a5
ICMDAW1H <48095884+ICMDAW1H@users.noreply.github.com>
2020-12-01 10:09:03 +0100

    esto nunca lo entendere si le doy a sincronizar y ya deberia guardar

--- UTF4-EJER2/MainWindow.xaml.cs
+++ UTF4-EJER2/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿
+
 using System.Collections.Generic;
 
 using System.Windows;
@@ -12,15 +13,90 @@ namespace UTF4_EJER2
         
 
         List <Superheroe> superheroes;
+        Superheroe superheroe;
         
         public MainWindow()
         {
 
-            this.superheroes = Superheroe.GetSamples();
-            verGrid.DataContext = superheroes;
-            TotalTextBlock.Text = superheroes.Count.ToString();
-
             InitializeComponent();
+            this.superheroes = Superheroe.GetSamples();
+
+            superheroe = new Superheroe { heroe = true };
+            verGrid.DataContext = superheroes[0];
+            CrearGrid.DataContext = superheroe;
+            TotalTextBlock.Text = superheroes.Count.ToString();
+            posicionTextBlock.Text = "1";
+            
+        }
+
+        private void izqflechaImage_MouseLeftButtonUp(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            int posicion, total;
+            total = superheroes.Count;
+            posicion = int.Parse(posicionTextBlock.Text);
+
+            if (posicion  > total)
+                drflechaImage.Visibility = Visibility.Collapsed;
+            else
+                drflechaImage.Visibility = Visibility.Visible;
+            if (posicion-1 == 1)
+                izqflechaImage.Visibility = Visibility.Collapsed;
+            else
+                izqflechaImage.Visibility = Visibility.Visible;
+            if (posicion <= 1)
+                return;
+            verGrid.DataContext = superheroes[posicion - 2];
+            posicionTextBlock.Text = (posicion-1).ToString();
+           
+
+        }
+
+        private void drflechaImage_PreviewMouseLeftButtonUp(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            int total, posicion;
+            total = superheroes.Count ;
+            posicion = int.Parse(posicionTextBlock.Text);
+
+            if (posicion == total)
+                izqflechaImage.Visibility = Visibility.Collapsed;
+            else
+                 izqflechaImage.Visibility = Visibility.Visible;
+
+            if (posicion + 1 == total)
+                drflechaImage.Visibility = Visibility.Collapsed;
+            else
+                drflechaImage.Visibility = Visibility.Visible;
+
+            if (posicion+1 > total)
+                return;
+            verGrid.DataContext = superheroes[posicion ];
+            posicionTextBlock.Text = (posicion + 1).ToString();
+            
+          
+        }
+
+        private void acepButton_Click(object sender, RoutedEventArgs e)
+        {
+
+            superheroes.Add(superheroe);
+            superheroe = new Superheroe{ heroe=true};
+            CrearGrid.DataContext = superheroe;
+            int total = superheroes.Count;
+            TotalTextBlock.Text = total.ToString();
+
+
+        }
+
+        private void limpButton_Click(object sender, RoutedEventArgs e)
+        {
+            superheroe = new Superheroe { heroe = true };
+
+        }
+
+        private void VengadoresCheckbox_Checked(object sender, RoutedEventArgs e)
+        {
+            superheroe.vengador = false;
+            superheroe.xmen = false;
         }
     }
 }
--- UTF4-EJER2/Superheroe.cs
+++ UTF4-EJER2/Superheroe.cs
@@ -1,74 +1,86 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 
-class Superheroe : INotifyPropertyChanged
+namespace UTF4_EJER2
 {
-    //no va lo del propfull asi que a mano 
 
-    private string Nombre ;
-    private string Imagen;
-    private bool Vengador;
-    private bool Xmen;
-    private bool Heroe;
-    private bool Villano;
-
-    public string nombre
+    class Superheroe : INotifyPropertyChanged
     {
-        get { return Nombre; }
-        set {
-            Nombre = value;
+        //no va lo del propfull asi que a mano 
+
+        private string Nombre;
+        private string Imagen;
+        private bool Vengador;
+        private bool Xmen;
+        private bool Heroe;
+        private bool Villano;
+
+
+
+        public string nombre
+        {
+            get { return Nombre; }
+            set
+            {
+                Nombre = value;
                 this.NotifyPropertyChanged("nombre");
+            }
+
         }
-        
-    } 
-    public string imagen
-    {
-        get { return Imagen; }
-        set {
-            Imagen = value;
+        public string imagen
+        {
+            get { return Imagen; }
+            set
+            {
+                Imagen = value;
                 this.NotifyPropertyChanged("imagen");
+            }
+
         }
-        
-    }
-    public bool vengador
-    {
-        get { return Vengador; }
-        set {
-            Vengador = value;
+        public bool vengador
+        {
+            get { return Vengador; }
+            set
+            {
+                Vengador = value;
                 this.NotifyPropertyChanged("vengador");
+            }
+
         }
-        
-    }
-    public bool heroe
-    {
-        get { return Heroe; }
-        set {
-            Heroe = value;
+        public bool heroe
+        {
+            get { return Heroe; }
+            set
+            {
+                Heroe = value;
                 this.NotifyPropertyChanged("heroe");
+            }
+
         }
-        
-    }
-    public bool villano
-    {
-        get { return Villano; }
-        set {
-            Villano = value;
+        public bool villano
+        {
+            get { return Villano; }
+            set
+            {
+                Villano = value;
                 this.NotifyPropertyChanged("villano");
+            }
+
         }
-        
-    }
-    public bool xmen
-    {
-        get { return Xmen; }
-        set {
-            Xmen = value;
+        public bool xmen
+        {
+            get { return Xmen; }
+            set
+            {
+                Xmen = value;
                 this.NotifyPropertyChanged("xmen");
+            }
+
         }
-        
-    }
-  
+
         public Superheroe()
         {
+            
         }
 
         public Superheroe(string nombre, string imagen, bool vengador, bool xmen, bool heroe, bool villano)
@@ -79,17 +91,17 @@ class Superheroe : INotifyPropertyChanged
             Xmen = xmen;
             Heroe = heroe;
             Villano = villano;
-    }
-    
+        }
 
 
-    public static List<Superheroe> GetSamples()
+
+        public static List<Superheroe> GetSamples()
         {
             List<Superheroe> ejemplos = new List<Superheroe>();
 
-            Superheroe ironman = new Superheroe("Ironman", @"https://sm.ign.com/ign_latam/screenshot/default/ybbpqktez5whedr0-1592031889_31aa.jpg",true, false,true,false);
-            Superheroe kingpin = new Superheroe("Kingpin", @"https://www.comicbasics.com/wp-content/uploads/2017/09/Kingpin.jpg", false, false,false,true);
-            Superheroe spiderman = new Superheroe("Spiderman", @"https://wipy.tv/wp-content/uploads/2019/08/destino-de-%E2%80%98Spider-Man%E2%80%99-en-los-Comics.jpg", true, true,true,false);
+            Superheroe ironman = new Superheroe("Ironman", @"https://sm.ign.com/ign_latam/screenshot/default/ybbpqktez5whedr0-1592031889_31aa.jpg", true, false, true, false);
+            Superheroe kingpin = new Superheroe("Kingpin", @"https://www.comicbasics.com/wp-content/uploads/2017/09/Kingpin.jpg", false, false, false, true);
+            Superheroe spiderman = new Superheroe("Spiderman", @"https://wipy.tv/wp-content/uploads/2019/08/destino-de-%E2%80%98Spider-Man%E2%80%99-en-los-Comics.jpg", true, true, true, false);
 
             ejemplos.Add(ironman);
             ejemplos.Add(kingpin);
@@ -97,12 +109,13 @@ class Superheroe : INotifyPropertyChanged
 
             return ejemplos;
         }
-    //Implementación de la interfaz INotifyPropertyChanged
-    public event PropertyChangedEventHandler PropertyChanged;
+        //Implementación de la interfaz INotifyPropertyChanged
+        public event PropertyChangedEventHandler PropertyChanged;
 
-    public void NotifyPropertyChanged(string propertyName)
-    {
-        this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        public void NotifyPropertyChanged(string propertyName)
+        {
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
     }
-
 }
