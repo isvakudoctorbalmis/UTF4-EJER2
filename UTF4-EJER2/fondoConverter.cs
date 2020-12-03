305472cdeaeb31bcee61038ffb6575958a4ca484
ICMDAW1H <48095884+ICMDAW1H@users.noreply.github.com>
2020-12-03 13:43:06 +0100

    No va el estatic resource despues de comer le hechare otro vistazo

---
+++ UTF4-EJER2/fondoConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace UTF4_EJER2
+{
+    public class fondoConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {             
+            
+            String color="";
+            if ((bool)value)
+            {
+                color = "PaleGreen";
+            }
+            else
+                color = "IndianRed";
+
+            return color;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ UTF4-EJER2/visibilidadConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+using System.Globalization;
+
+using System.Windows.Data;
+
+namespace UTF4_EJER2
+{
+    public class visibilidadConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            String visibiladad = "";
+            if ((bool)value)
+            {
+                visibiladad = "Visible";
+            }
+            else
+                visibiladad = "Collapsed";
+
+            return visibiladad;
+
+
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
